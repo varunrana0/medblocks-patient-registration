7e4e9a1a74f58fc81056cb919a635635f8c60d48
varun rana <varunrana829@gmail.com>
2025-05-18 11:26:07 +0530

    feat: setup local Pglite database connection

---
+++ lib/db.ts
@@ -0,0 +1,47 @@
+import path from "path";
+import { PGlite } from "@electric-sql/pglite";
+import { existsSync, mkdirSync } from "fs";
+
+let db: PGlite | null = null;
+const dbpath = path.join(process.cwd(), "db", "patients");
+
+export async function getDbConnection() {
+  try {
+    if (!db) {
+      if (!existsSync(dbpath)) {
+        mkdirSync(dbpath, { recursive: true });
+      }
+
+      db = new PGlite(dbpath);
+      console.log("Waiting for DB to be ready", await db.waitReady);
+    }
+
+    await initiateDB(db);
+    console.log("Connected to DB");
+    return db;
+  } catch (error) {
+    console.error("Error connecting to DB:", error);
+  }
+}
+
+async function initiateDB(db: PGlite) {
+  try {
+    await db.exec(`
+    CREATE TABLE IF NOT EXISTS patients (
+      id SERIAL PRIMARY KEY,
+      firstName TEXT NOT NULL,
+      lastName TEXT NOT NULL,
+      age INTEGER NOT NULL,
+      gender TEXT CHECK(gender IN ('Male', 'Female', 'Other')) NOT NULL,
+      contactNumber TEXT NOT NULL,
+      email TEXT,
+      address TEXT,
+      bloodGroup TEXT,
+      medicalConditions TEXT, -- store as JSON string
+      createdAt TEXT NOT NULL -- ISO string
+    );
+  `);
+  } catch (error) {
+    console.error("Error creating table:", error);
+  }
+}
